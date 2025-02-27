fb7315110a7f586ae3f2bbfc36177e24bb197818
rawanabdulhayy <rawanabdulhay29700@gmail.com>
2025-02-27 17:21:17 +0200

    added the auth possible states

---
+++ lib/features/authentication/domain/BLoc/authentication_state.dart
@@ -0,0 +1,14 @@
+part of 'authentication_cubit.dart';
+
+@immutable
+sealed class AuthenticationState {}
+
+final class AuthenticationInitial extends AuthenticationState {}
+
+class AuthInitial extends AuthenticationState {} // Initial state before authentication starts.
+
+class AuthLoading extends AuthenticationState {} // State when authentication is in progress.
+
+class AuthAuthenticated extends AuthenticationState {} // State when authentication succeeds.
+
+class AuthFailed extends AuthenticationState {} // State when authentication fails.
